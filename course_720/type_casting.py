d46af87c6226c82fb2009282b2db481c695c61f0
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2024-09-04 17:18:11 -0500

    720 course type_casting: Added variables and their output data types

---
+++ course_720/type_casting.py
@@ -0,0 +1,17 @@
+# Typecasting is the process of converteing a variable from one data type to another
+#             str(), int(), float(), bool()
+
+
+name = "Cupahnoodle"
+platform = "Twitch"
+viewers = 1300
+subscribers = 10000
+is_online = True
+
+# To get the data type of a variable/value use the type function ---> type(variable)
+print(type(name))
+print(type(platform))
+print(type(viewers))
+print(type(subscribers))
+print(type(is_online))
+
