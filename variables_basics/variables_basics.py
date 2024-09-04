7ed6c45faec58bcab65eaca95e7172a2ce03a8b3
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2024-09-04 01:46:40 -0500

    this commit is to test if i changed my profile correctly

--- variables_basics/variables_basics.py
+++ variables_basics/variables_basics.py
@@ -1,6 +1,8 @@
 #This is a simple file added to make an initial commit
 age1 = 1 #valid
 age_2 = 2 #valid
+#age-3 = 3 #invalid ---> Hyphens and dashes cannot be used in variables
+#4age = 4 #invalid ---> Numbers cannot be used as the first character in a variablbe
 
 print(age1)
 print((age_2))
