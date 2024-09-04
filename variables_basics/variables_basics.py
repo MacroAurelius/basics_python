fe4c8c8161ac9fc8c91dd0c9c6b354210f063738
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2024-09-04 03:07:18 -0500

    720 course variables_basics additional naming conventions added

--- variables_basics/variables_basics.py
+++ variables_basics/variables_basics.py
@@ -1,8 +1,10 @@
 #This is a simple file added to make an initial commit
-age1 = 1 #valid
-age_2 = 2 #valid
+age1 = 1   #valid
+age_2 = 2  #valid
 #age-3 = 3 #invalid ---> Hyphens and dashes cannot be used in variables
-#4age = 4 #invalid ---> Numbers cannot be used as the first character in a variablbe
+#4age = 4 #invalid ---> Numbers cannot be used as the first character in the variablb
+Age = 5   #This is valid but isn't recommended to use an upper-case in the variable
+
 
 print(age1)
 print((age_2))
