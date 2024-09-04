e9a19448f7493ec7748d390012f71b5462393f89
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2024-09-04 17:46:43 -0500

    720 course type_casting: Added variables and their output data types

--- course_720/type_casting.py
+++ course_720/type_casting.py
@@ -5,7 +5,8 @@
 name = "Cupahnoodle"
 platform = "Twitch"
 viewers = 1300
-subscribers = 10000
+live_time = 2.5
+subscribers = 15000
 is_online = True
 
 # To get the data type of a variable/value use the type function ---> type(variable)
@@ -15,3 +16,20 @@ print(type(viewers))
 print(type(subscribers))
 print(type(is_online))
 
+# Using these type cast unctions ---> str(), int(), float(), bool() --- we can convert to one data type to another
+
+# converting from a float to a integer
+live_time_int = int(live_time)
+print(live_time)
+print(live_time_int)
+
+# converting from an integer to a float
+subscribers_float = float(subscribers)
+print(subscribers)
+print(subscribers_float)
+
+# converting int() to str()
+viewers_str = str(viewers)
+print(viewers_str)
+print(type(viewers))
+print(type(f"Type String: {viewers_str}"))
