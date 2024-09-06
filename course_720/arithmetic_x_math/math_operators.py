398cf45f6dc7faa67de4efb7b1e3a59f7f14a591
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2024-09-05 19:59:30 -0500

    720 course arithmetic and math: Built in Math Related opt

--- course_720/arithmetic_x_math/math_operators.py
+++ course_720/arithmetic_x_math/math_operators.py
@@ -1,4 +1,4 @@
-
+import math
 
 friends = 0
 
@@ -13,4 +13,30 @@ remainder = friends % 2 # Modulus
 
 print(friends)
 print(remainder)
+
+#Built in Math related options
+
+x = 3.14
+y = 4
+z = 5
+
+#Round Functions: round(number, ndigits)
+round_result = round(x)
+print(f"The round for x is: {round_result}")
+
+#Absolute Value Functions: abs(__x)
+y = -4
+absolute_result = abs(y)
+print(f"The absolute value of y is: {absolute_result}")
+
+#Power Function: pow(base, exponent, mod)
+power_result = pow(4, 3)
+
+#Max Function: max()
+max_result = max(x, y, z)
+print(f"The maximum value between x, y, and z is: {max_result}")
+
+#Minimum Function: min()
+min_result = min(x, y, z)
+print(f"The minimum value btween x. y, and z is: {min_result}")
 
