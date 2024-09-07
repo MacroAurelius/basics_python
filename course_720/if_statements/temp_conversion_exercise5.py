f2c21ad40093d8b4fb851e68ac843c5afb882cdc
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2024-09-07 13:34:44 -0500

    720 course if_statements: Completed Temperature Conversion Exercise (Fahrenheit condition)

--- course_720/if_statements/temp_conversion_exercise5.py
+++ course_720/if_statements/temp_conversion_exercise5.py
@@ -9,6 +9,7 @@ if unit == "C":
     temp = round((9 * temp) / 5 + 32, 1)
     print(f"The temperature in Fahrenheit is: {temp}{degree_symbol}F")
 elif unit == "F":
-    pass
+    temp = round((temp -32) * 5 / 9, 1)
+    print(f"The temperature in Celsius is: {temp}{degree_symbol}C")
 else:
     print(f"{unit} is an invalid unit of measurement")
