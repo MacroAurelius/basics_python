1a583ef44da862a122c9d37ac4931b0e1fa7e0b9
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2024-09-07 13:31:55 -0500

    720 course if_statements: Temperature Conversion Exercise (Celsius added and degree symbol added)

--- course_720/if_statements/temp_conversion_exercise5.py
+++ course_720/if_statements/temp_conversion_exercise5.py
@@ -2,11 +2,12 @@
 
 unit = input("Is this temperature in Celsius or Fahrenheit (C/F): ")
 temp = float(input("Enter the temperature: "))
+degree_symbol = "\u00b0"
 
 # While creating if statements, you can use the keyword PASS to be a space filler until you set the condition
 if unit == "C":
     temp = round((9 * temp) / 5 + 32, 1)
-    print(f"The temperature in Fahrenheit is: {temp}F")
+    print(f"The temperature in Fahrenheit is: {temp}{degree_symbol}F")
 elif unit == "F":
     pass
 else:
