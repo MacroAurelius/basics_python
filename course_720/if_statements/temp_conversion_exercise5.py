30cfb9804abd082027500b17af4658af3a2c8142
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2024-09-07 13:15:56 -0500

    720 course if_statements: Temperature Conversion Exercise (pass condition example)

---
+++ course_720/if_statements/temp_conversion_exercise5.py
@@ -0,0 +1,12 @@
+# Exercise 5: Temperature Conversion
+
+unit = input("Is this temperature in Celsius or Fahrenheit (C/F): ")
+temp = float(input("Enter the temperature: "))
+
+# While creating if statements, you can use the keyword PASS to be a space filler until you set the condition
+if unit == "C":
+    pass
+elif unit == "F":
+    pass
+else:
+    print(f"{unit} is an invalid unit of measurement")
