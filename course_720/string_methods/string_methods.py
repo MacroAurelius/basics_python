9dc8830617ba822607dbbdbec6c12beb19552183
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2024-09-10 21:51:19 -0500

    720 course string methods: Added Find Method with examples

--- course_720/string_methods/string_methods.py
+++ course_720/string_methods/string_methods.py
@@ -6,5 +6,12 @@ name = input("Enter your full name: ")
 len_result = len(name)  # store the len() within a variable
 print(len_result)
 
-
+# FIND Method ---> .find()
+# The first occurrence/first position of a given character
+find_result = name.find(" ")  # this will return the position of the FIRST space in the user's input
+print(find_result)
+# To find the last occurrence ---> .rfind()
+last_result = name.rfind(" ")  # this will return the position of the LAST space in the user's input
+print(last_result)
+# NOTE: If there is no occurrence in the string, then it will return negative one (-1)
 
