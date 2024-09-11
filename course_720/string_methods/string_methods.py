eca2a0c5f167b390cda91ce3bf14e3237e3c35e4
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2024-09-10 22:11:01 -0500

    720 course string methods: added Upper and Lower case methods w examples

--- course_720/string_methods/string_methods.py
+++ course_720/string_methods/string_methods.py
@@ -6,18 +6,25 @@ name = input("Enter your full name: ")
 len_result = len(name)  # store the len() within a variable
 print(len_result)
 
-# FIND Method ---> .find()
+# The FIND Method ---> .find()
 # The first occurrence/first position of a given character
 find_result = name.find(" ")  # this will return the position of the FIRST space in the user's input
 print(find_result)
-
 # To find the last occurrence ---> .rfind()
 last_result = name.rfind(" ")  # this will return the position of the LAST space in the user's input
 print(last_result)
 # NOTE: If there is no occurrence in the string, then it will return negative one (-1)
 
-# Capitalization Method
+
+# The CAPITALIZATION Method ---> .capitalize()
 # To capitalize the firs letter in the string
 cap_name = name.capitalize()
 print(cap_name)  # this only capitalizes the first letter of the string
 
+# The UPPER Method
+upper_name = name.upper()
+print(upper_name)
+
+# The LOWER Method ---> .lower()
+lower_name = name.lower()
+print(lower_name)
