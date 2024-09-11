e7f8bee026d79dcc1e4af93cfd3ee0eeed9858a2
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2024-09-10 21:38:20 -0500

    720 course string methods: Added length function with example

---
+++ course_720/string_methods/string_methods.py
@@ -0,0 +1,10 @@
+# A String is just a series of characters
+
+name = input("Enter your full name: ")
+
+# The LENGTH function will give you the length of the string ---> len()
+len_result = len(name)  # store the len() within a variable
+print(len_result)
+
+
+
