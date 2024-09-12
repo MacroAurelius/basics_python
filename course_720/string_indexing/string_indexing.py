fc18659ac9221b371bb7461053db9f41de9bab21
2xiix2 <122521905+2xiix2@users.noreply.github.com>
2024-09-11 20:21:52 -0500

    720 course string indexing: Added examples of the index of a credit card number

---
+++ course_720/string_indexing/string_indexing.py
@@ -0,0 +1,10 @@
+# indexing = accessing elements of a sequence using [] (indexing operator)
+#            [start : end : step] (We can access a staring point, an end point, or points at each step)
+
+credit_number = "1234-5678-9012-3456"
+
+# Note that the index count starts at 0 (example below)
+print(f"The first character of the credit card number is: {credit_number[0]}")
+print(f"The second: {credit_number[1]}")
+print(f"The third: {credit_number[2]}")
+print(f"At the ninth index of the card number: {credit_number[9]}")
